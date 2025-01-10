22b3f0f2d8038449782603820b35eb5c4054725b
sshrey15 <stud22.sks2@gec.ac.in>
2025-01-10 12:44:39 +0530

    loader#5

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -141,7 +141,7 @@ const UploadPage = () => {
 
 
   return (
-    <div className="relative w-full min-h-screen">
+    <div className="relative w-full min-h-screen pt-16">
 
       {/* Background with opacity */}
       <div className="absolute top-0 left-0 w-full h-full z-0">
