049de3bee61b4d77d1e20d3d898050f479872ee1
sshrey15 <stud22.sks2@gec.ac.in>
2025-01-24 00:21:48 +0530

    folder_architecture#2

--- src/app/api/imagemanager/[id]/route.ts
+++ src/app/api/imagemanager/[id]/route.ts
@@ -1,6 +1,6 @@
-import {NextResponse} from "next/server";
-import {PrismaClient} from "@prisma/client"
-import * as cloudinary from "cloudinary"
+import {NextResponse, NextRequest} from "next/server";
+import {PrismaClient} from "@prisma/client";
+import * as cloudinary from "cloudinary";
 
 const prisma = new PrismaClient();
 
@@ -8,26 +8,26 @@ cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
     api_key: process.env.CLOUDINARY_API_KEY || "",
     api_secret: process.env.CLOUDINARY_API_SECRET || "",
-})
+});
 
-export async function GET(req: Request, {params}: {params: {id: string}}){
+export async function GET(req: NextRequest, {params}: {params: {id: string}}){
     const id = params.id;
     try{
         const gallery = await prisma.gallery.findUnique({
             where: {id},
-        })
+        });
 
         if(!gallery){
             return NextResponse.json(
                 {message: "Gallery not found"},
                 {status: 404}
-            )
+            );
         }
 
         return NextResponse.json({
             message: "success",
             data: gallery,
-        })
+        });
 
     }catch(err){
         console.error("API error: ", err);
@@ -35,7 +35,7 @@ export async function GET(req: Request, {params}: {params: {id: string}}){
         return NextResponse.json(
             {message: "error", error: errorMessage},
             {status: 500}
-        )
+        );
     } finally{
         await prisma.$disconnect();
     }
